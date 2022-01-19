d648e5d2fbe8a61d91cbf9844ca6d6c48364e9cf
Amuuuuuuuu <hyakki@hyakkide-iMac.Home>
2022-01-20 00:46:24 +0800

    第三天

--- src/route.js
+++ src/route.js
@@ -6,6 +6,18 @@ const pageData = [
   { id: 'forget-password-step-1', label: '忘記密碼-step-1' },
   { id: 'forget-password-step-2', label: '忘記密碼-step-1' },
   { id: 'forget-password-step-3', label: '忘記密碼-step-1' },
+ 
+  // { id: 'index', label: '首頁' },
+  { id: 'index', label: '露西面試助理' },
+  { id: 'opening-website', label: '徵才網站' },
+  { id: 'dropdown', label: '職缺' },
+  { id: 'candidate-response', label: '面試者資料' },
+  { id: 'plan', label: '方案' },
+  // { id: '', label: '露西交流網' },
+  { id: 'account-setting', label: '設定' },
+  { id: 'record', label: '交易紀錄' },
+  { id: 'user', label: '應徵頁面' },
+  { id: '404', label: '404' },
 ]
 
 const PAGE_LIST = []
